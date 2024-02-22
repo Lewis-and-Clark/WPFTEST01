6b86940f630d19164cec4cdec6b5f1c53703a299
ChaosCow58 <101840562+ChaosCow58@users.noreply.github.com>
2024-02-22 09:16:01 -0600

    Add project files.

---
+++ WPFTEST01/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Forms.Integration;
+
+namespace WPFTEST01
+{
+    public partial class MainWindow : Window
+    {
+        private Process process;
+        private WindowsFormsHost host;
+        private ConsoleControl
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
